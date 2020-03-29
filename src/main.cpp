069f1eae05e9c0f0805738f94a0146b24e070f9a
Eduardo Martinez <tedmar@gmail.com>
2020-03-29 17:23:25 -0300

    First 18B20 program

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,23 @@
 
 #include <Arduino.h>
 
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
 //  Definitions
 
 /*
  *  Defined in plaformio.ini
  *  BLINK_LED   IOport for LED
  *  SERIAL_BAUD Serial baud rate
+ *  ONE_WIRE_BUS    IOport for 18B20
  */
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
 
 const int tblink = 1000;
 
@@ -20,16 +30,26 @@ setup(void)
 {
     pinMode(BLINK_LED, OUTPUT);
     Serial.begin(SERIAL_BAUD);
+    // Start the DS18B20 sensor
+    sensors.begin();
+    digitalWrite(BLINK_LED,OFF_LED);
+
+}
+
+static void
+show_temperature(void)
+{
+    sensors.requestTemperatures();
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.println(" ºC");
 }
 
 void
 loop(void)
 {
-    digitalWrite(BLINK_LED,HIGH);
-    Serial.println("on");
+    digitalWrite(BLINK_LED,ON_LED);
+    show_temperature();
+    digitalWrite(BLINK_LED,OFF_LED);
     delay(tblink);
-
-    digitalWrite(BLINK_LED,LOW);
-    Serial.println("off");
-    delay(tblink/2);
 }
+
